7016e468001b8101c44fedcf923046210ab71a66
Vera <Vsimonova14@mail.ru>
2022-12-09 15:16:39 +0300

    create block with promo

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.owl_carousel').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    })
+})
